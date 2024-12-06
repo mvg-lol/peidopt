2f5c01d7cb2fe55c13ff0696c57ba31972c67da8
marto <67065789+Marte77@users.noreply.github.com>
2024-12-06 23:48:56 +0000

    n tirei o control

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function addAudio() {
     document.getElementsByTagName("body")[0].innerHTML = `
-        <audio loop autoplay controls>
+        <audio loop autoplay>
             <source src="wetfart.mp3" type="audio/filetype">
         </audio>
         ${document.getElementsByTagName("body")[0].innerHTML}
