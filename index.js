0d945adbc136faebbf8a64e26816f1a079d9d650
marto <67065789+Marte77@users.noreply.github.com>
2024-12-07 00:07:44 +0000

    peido bug

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 var isDone = false
 function addAudio() {
     if (!isDone)
-        document.getElementsByTagName("body")[0].innerHTML = `
+    {    document.getElementsByTagName("body")[0].innerHTML = `
             <audio loop autoplay>
                 <source src="wetfart.mp3" type="application/ogg">
             </audio>
             ${document.getElementsByTagName("body")[0].innerHTML}
             `
+            isDone = true
+    }
+    
 }
