e37892622f50635a718ccfb11c9cddd07c0f8a54
martinho <eu@martinho.pt>
2024-12-07 01:04:25 +0000

    peido

--- index.js
+++ index.js
@@ -3,15 +3,7 @@ function addAudio() {
     console.log("boas")
     if (!isDone)
     {
-        document.getElementsByTagName("body")[0].innerHTML = `
-            <audio loop autoplay>
-                <source src="wetfart.mp3" type="application/ogg">
-            </audio>
-            ${document.getElementsByTagName("body")[0].innerHTML}
-            `
         isDone = true
+        document.getElementById("audio").play()
     }
-    
-    //document.getElementById("p").innerHTML = isDone ? "ola" : "xau"
-    //isDone = !isDone
 }
