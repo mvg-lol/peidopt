7fcf72f85787b041a1eb89f945984a6f6919b27d
marto <67065789+Marte77@users.noreply.github.com>
2025-01-30 18:44:55 +0000

    Update index.js

--- index.js
+++ index.js
@@ -4,14 +4,6 @@ function addAudio() {
     if (!isDone)
     {
         isDone = true
-        //document.getElementById("audio").play()
-        removeBodyAndAddConnections()
+        document.getElementById("audio").play()
     }
-}
-
-function removeBodyAndAddConnections() {
-    let body = document.getElementById("body")
-    body.innerHTML = `
-    <iframe style="position:fixed; top:0; left:0; bottom:0; right:0; width:100%; height:100%; border:none; margin:0; padding:0;" title="Conexões" src="https://mvg.lol/joguinhos/"></iframe>
-    `
 }
