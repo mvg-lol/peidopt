21328cab9d073add91ed3c88750db64f0742e26c
marto <martinho.tavares@live.com.pt>
2025-04-15 11:59:49 +0100

    #1 impl

--- index.js
+++ index.js
@@ -7,3 +7,12 @@ function addAudio() {
         document.getElementById("audio").play()
     }
 }
+
+let createRayId = () => {
+    let d = document.getElementById("rayid")
+    console.log(d)
+    if (d !== null) {
+        d.textContent = "Ray ID: " + crypto.randomUUID().split("-").filter((val,index)=>{return index < 4}).join("")
+    }
+}
+createRayId()
